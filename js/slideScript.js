02f3abe5b5f0b9f1aa006f8aa7286bcd822ac6fb
Mark Lavrentyev <lavrema@outlook.com>
2017-08-28 09:17:37 -0400

    Add shifting images to page

--- js/slideScript.js
+++ js/slideScript.js
@@ -1,22 +1,32 @@
 var curPhoto = -1;
-var slideTime = 1000;
+var slideTime = 5000;
 var timeToSwitch = 0;
 
+function updateSlide() {
+    $("#photoTitle").text(imgList[curPhoto].imgTitle);
+    $("#photos img").css('display', 'none');
+    $("#photos img[num=\"" + curPhoto + "\"]").css('display', 'block');
+}
+
 $(function(){
-    // Initialize the dots and the images
+    // Initialize the dots and images
     var numDots = 0;
-    for(img in imgList) {
+    imgList.forEach(function(img){
         $("#dotContainer").append("<div class=\"dot\" num=\"" + numDots + "\"></div>");
+        $("#photos").append("<img src=\"img/homeSlides/" + img.imgFile + "\" num=\"" + numDots + "\" />");
         numDots++;
-    }
+    });
     
+    // Click on dots
     $(".dot").click(function(){
         $(".dot").css('background-color', '#999999');
         $(this).css('background-color', 'black');
         timeToSwitch = new Date().getTime() + slideTime;
         curPhoto = parseInt($(this).attr("num"));
+        updateSlide();
     });
     
+    // Periodically change image
     setInterval(function(){
         time = new Date().getTime();
         if(time > timeToSwitch) {
@@ -24,7 +34,9 @@ $(function(){
             curPhoto = (curPhoto + 1) % numDots;
 
             $(".dot").css('background-color', '#999999');
-            $("[num=" + curPhoto + "]").css('background-color', 'black');
+            $(".dot[num=" + curPhoto + "]").css('background-color', 'black');
+            updateSlide();
         }
+                
     }, 500);
 });
