34329b7532e5f120debad2d8602a6b1356ef4be0
Mark Lavrentyev <lavrema@outlook.com>
2017-10-24 11:55:22 -0400

    Add js files to the projects website to load stuff

---
+++ js/projects.js
@@ -0,0 +1,3 @@
+$(function() {
+    console.log(projs[0]);
+});
---
+++ js/projs.js
@@ -0,0 +1,22 @@
+projs = [
+    {
+        'name': 'MNIST Neural Network',
+        'url': 'https://github.com/MLavrentyev/MNIST-Neural-Net',
+        'color': '#c2c2c2',
+    },
+    {
+        'name': 'City New Tab',
+        'url': 'https://github.com/MLavrentyev/NewTab',
+        'color': '#ffa244',
+    },
+    {
+        'name': 'Snake',
+        'url': 'https://github.com/MLavrentyev/Snake',
+        'color': '#b66c20',
+    },
+    {
+        'name': 'FRC Team Scheduler',
+        'url': 'https://github.com/MLavrentyev/FRC-Team-Scheduler',
+        'color': '#58a9ff',
+    },
+];
