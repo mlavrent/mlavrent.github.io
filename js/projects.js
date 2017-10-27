f1f4be975a8059ea778c5938ba154ce4edf25b58
Mark Lavrentyev <lavrema@outlook.com>
2017-10-26 22:26:44 -0400

    Write script to add projects from text file

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,8 @@
 $(function() {
     console.log(projs[0]);
+    for(var proj of projs) {
+        var newString = "<a href='" + proj['url'] + "' style='background: linear-gradient(180deg, " + proj['color'] + " 60%, white 40%);' class='projBox'><h3>" + proj['name'] + "</h3></a>";
+        console.log(newString);
+        $("#projTable").append(newString);
+    }
 });
