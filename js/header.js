b5a9f0a8c99f0aa7c9dbd5191cb8c369f4833bee
Mark Lavrentyev <lavrema@outlook.com>
2019-05-21 17:23:46 +0300

    Revamp site and set up new header

---
+++ js/header.js
@@ -0,0 +1,57 @@
+
+$(setHeader);
+$(window).resize(setHeader);
+
+function setHeader() {
+    let name = $("#profileName");
+    let pic = $("#profilePic");
+
+    if ($(window).width() < 440) {
+        name.text("mark");
+        showHamburger();
+        shrinkTitle();
+    } else if ($(window).width() < 850) {
+        name.text("mark");
+        showHamburger();
+        growTitle();
+    } else if ($(window).width() < 1150) {
+        name.text("mark");
+        hideHamburger();
+        growTitle();
+    } else {
+        name.text("mlavrentyev");
+        hideHamburger();
+        growTitle();
+    }
+}
+
+function hideHamburger() {
+    $("#links").show();
+    $("#hamburgerIcon").hide();
+}
+function showHamburger() {
+    $("#links").hide();
+    $("#hamburgerIcon").show();
+}
+
+function shrinkTitle() {
+    let name = $("#profileName");
+    let pic = $("#profilePic");
+
+    name.css("fontSize", 50);
+    pic.css("height", 60);
+    pic.css("width", 60);
+}
+function growTitle() {
+    let name = $("#profileName");
+    let pic = $("#profilePic");
+
+    name.css("fontSize", 60);
+    pic.css("height", 100);
+    pic.css("width", 100);
+}
+
+
+$("#hamburgerIcon").click(function() {
+    $("#hamburgerMenu")
+});
--- js/projects.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    console.log(projs[0]);
-    for(var proj of projs) {
-        var newString = "<a href='" + proj['url'] + "' style='background: linear-gradient(180deg, " + proj['color'] + " 60%, white 40%);' class='projBox'><h3>" + proj['name'] + "</h3></a>";
-        console.log(newString);
-        $("#projTable").append(newString);
-    }
-});
-// TODO: figure out why single-line boxes have margins???
